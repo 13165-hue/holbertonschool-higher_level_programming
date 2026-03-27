15a98df4e683b4e87cf244f96d6aa4c04be8a783
13165-hue <13165-hue@holbertonstudents.com>
2026-03-27 18:57:20 +0000

    Add task 7: error code requests

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Sends a request to a URL and displays the body or HTTP error code."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
