93ff6f1df24e3a491f5c493821e03b174a82012f
13165-hue <13165-hue@holbertonstudents.com>
2026-03-27 17:49:00 +0000

    Clean content fix

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
+"""
+Fetches https://intranet.hbtn.io/status
+"""
 import urllib.request
 
+
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
