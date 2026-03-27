7fb4144573ea00cab6374057b19d5a48058aca5a
13165-hue <13165-hue@holbertonstudents.com>
2026-03-27 19:01:19 +0000

    Add task 10: github commits

---
+++ python-network_1/100-github_commits.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Lists the 10 most recent commits of a GitHub repository."""
+import requests
+import sys
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    r = requests.get(url, params={'per_page': 10})
+    for commit in r.json():
+        sha = commit.get('sha')
+        name = commit.get('commit').get('author').get('name')
+        print("{}: {}".format(sha, name))
