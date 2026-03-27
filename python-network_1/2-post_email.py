5509feadb7fbfbf76c9caa9bf0b2077f67145db8
13165-hue <13165-hue@holbertonstudents.com>
2026-03-27 18:53:56 +0000

    Fix: protect code with __main__ guard

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -4,10 +4,11 @@ import urllib.request
 import urllib.parse
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
-with urllib.request.urlopen(url, data) as response:
-    print(response.read().decode('utf-8'))
+    with urllib.request.urlopen(url, data) as response:
+        print(response.read().decode('utf-8'))
