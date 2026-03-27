82c7a0bfe78e0a20720bd2cc3f9fd75d9e051c47
13165-hue <13165-hue@holbertonstudents.com>
2026-03-27 18:04:37 +0000

    Final check: Correct file content and location

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,7 +6,8 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
