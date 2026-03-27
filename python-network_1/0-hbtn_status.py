bdfc145332a2bc440d4c38c5a7d511f0fd36fc64
13165-hue <13165-hue@holbertonstudents.com>
2026-03-27 17:41:04 +0000

    Initial commit with README and Task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""
-Python script that fetches https://intranet.hbtn.io/status
-"""
+"""Fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
-
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
