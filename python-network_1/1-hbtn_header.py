8780ad3ee0cbe449a47cae038f170cfc858dfd9b
13165-hue <13165-hue@holbertonstudents.com>
2026-03-27 18:58:44 +0000

    Fix task 1: add main guard and docstring

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Displays the value of X-Request-Id from the response header"""
+"""Displays the value of X-Request-Id from the response header."""
 import urllib.request
 import sys
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers.get('X-Request-Id'))
